0200399a304a404030567b456d477763f9208701
Chris Egerton <chrise@aiven.io>
2024-08-05 20:36:39 +0200

    MINOR: Fix stage recording in DistributedHerder::addRequest (#16784)
    Reviewers: Greg Harris <greg.harris@aiven.io>
    (cherry picked from commit 3f3e3767976159d1291d81699d53618c3cc224eb)

--- connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/DistributedHerder.java
+++ connect/runtime/src/main/java/org/apache/kafka/connect/runtime/distributed/DistributedHerder.java
@@ -2364,6 +2364,7 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
     }
 
     DistributedHerderRequest addRequest(long delayMs, Callable<Void> action, Callback<Void> callback) {
+        callback.recordStage(tickThreadStage);
         DistributedHerderRequest req = new DistributedHerderRequest(time.milliseconds() + delayMs, requestSeqNum.incrementAndGet(), action, callback);
         requests.add(req);
         // We don't need to synchronize here
@@ -2375,7 +2376,6 @@ public class DistributedHerder extends AbstractHerder implements Runnable {
         // queue was added
         if (peekWithoutException() == req)
             member.wakeup();
-        callback.recordStage(tickThreadStage);
         return req;
     }
 
